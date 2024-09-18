0d756b517846cceabdf80251df4abb219c132dbb
Shalini Kushwaha <2637shalini@gmail.com>
2024-09-19 03:43:01 +0530

    updated experience and education files

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,9 +6,10 @@ function Education() {
             <section className="education">
                 <div className="timeline">
                     <div className="timeline-container leftc">
-                        <span className="dots"></span>
+                        <span className="dots">2022</span>
                         <div className="text-box">
-                            <h2>edfsdfuihh ytfi</h2>
+                            <h2>Bachelor of Technologies<br /> in Computer Science</h2>
+                            <small>Rajkiya Engineering College</small>
                             <p>
                                 fdj edf rdf resdfglh; trdfgh tyfglu terdyfug etrfi tryfugih
                                 etryfugh dfg tfdyfgu tdfg tddfj tryu trfgy fdxgch gfdhgj...
@@ -17,39 +18,30 @@ function Education() {
                         </div>
                     </div>
                     <div className="timeline-container rightc">
-                        <span className="dots"></span>
+                        <span className="dots">2018</span>
                         <div className="text-box">
-                            <h2>edfsdfuihh ytfi</h2>
-                            <small>asdsfsf</small>
+                            <h2>Higher Secondary School</h2>
+                            <small>Kendriya Vidyalaya</small>
                             <p>
-                                fdj edf rdf resdfglh; trdfgh tyfglu terdyfug etrfi tryfugih
-                                etryfugh dfg tfdyfgu tdfg tddfj tryu trfgy fdxgch gfdhgj...
+                                Specialization: Science (with Mathematics and Computer Science) <br />
+                                Secured 85% in the board examinations with distinction in Computer Science.
                             </p>
                             <span className="rightc-arrow" />
                         </div>
                     </div>
                     <div className="timeline-container leftc">
-                        <span className="dots"></span>
+                        <span className="dots">2016</span>
                         <div className="text-box">
                             <h2>edfsdfuihh ytfi</h2>
                             <p>
-                                fdj edf rdf resdfglh; trdfgh tyfglu terdyfug etrfi tryfugih
-                                etryfugh dfg tfdyfgu tdfg tddfj tryu trfgy fdxgch gfdhgj...
+                                Achieved a score of 90% in final board exams. <br />
+                                Excelled in academics, demonstrating strong problem-solving
+                                skills and a passion for learning.
                             </p>
                             <span className="leftc-arrow" />
                         </div>
                     </div>
-                    <div className="timeline-container rightc">
-                        <span className="dots"></span>
-                        <div className="text-box">
-                            <h2>edfsdfuihh ytfi</h2>
-                            <p>
-                                fdj edf rdf resdfglh; trdfgh tyfglu terdyfug etrfi tryfugih
-                                etryfugh dfg tfdyfgu tdfg tddfj tryu trfgy fdxgch gfdhgj...
-                            </p>
-                            <span className="rightc-arrow" />
-                        </div>
-                    </div>
+                    
                 </div>
             </section>
         </>
