a8d0560e4029a20ca5bea10a285772b9aa449c6c
Shalini Kushwaha <2637shalini@gmail.com>
2024-09-20 23:33:18 +0530

    updated section content

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,62 +5,70 @@ function Experience() {
     return (
         <>
             <section className="experience">
+                <div className="ex-bg-wrap">
+                    <div className="ex-bg"></div>
+
+                </div>
                 <div className="container flex-col">
                     <h1>Experience</h1>
 
                     <div className="ex-container flex-col">
                         <div className="ex-container-sub">
-                            <div className="ex-box" id="ex-box-1">
-                                <div className="ex-stats">
+                            <div className="ex-box flex-col" id="ex-box-1">
+                                <div className="ex-stats flex-row">
                                     <div className="ex-stats-figure">2</div>
                                     <div className="ex-stats-symbol">+</div>
                                 </div>
                                 <div className="ex-description">years</div>
                             </div>
-                            <div className="ex-box" id="ex-box-2">
-                                <div className="ex-stats">
+                            {/* <div className="card-bg"></div> */}
+                            <div className="ex-box flex-col" id="ex-box-2">
+                                <div className="ex-stats flex-row">
+                                    <div className="ex-stats-figure">8</div>
+                                    <div className="ex-stats-symbol mg-1rem">+</div>
+                                    <div className="ex-stats-figure mg-1rem">Successful</div>
+                                    <div className="ex-stats-figure">Collaborations</div>
+                                </div>
+                                <div className="ex-description">Collaborated with cross-functional teams to create innovative solutions. Excelled in Agile environments, ensuring seamless integration and consistently delivering high-impact results.</div>
+                            </div>
+                            <div className="ex-box flex-col" id="ex-box-3">
+                                <div className="ex-stats flex-row">
                                     <div className="ex-stats-figure">100</div>
                                     <div className="ex-stats-symbol">%</div>
                                 </div>
                                 <div className="ex-description">passion</div>
                             </div>
-                            <div className="ex-box" id="ex-box-3">
-                                <div className="ex-stats">
-                                    <div className="ex-stats-figure">8</div>
-                                    <div className="ex-stats-symbol">+</div>
-                                    <div className="ex-stats-figure">Successful</div>
-                                    <div className="ex-stats-figure">Collaborations</div>
-                                </div>
-                                <div className="ex-description">Collaborated with cross-functional teams to create innovative solutions. Excelled in Agile environments, ensuring seamless integration and consistently delivering high-impact results.</div>
-                            </div>
+
                         </div>
                         <div className="ex-container-sub">
-                            <div className="ex-box" id="ex-box-4">
+                            <div className="ex-box flex-col" id="ex-box-4">
 
-                                <div className="ex-stats-figure">Software Engineer</div>
+                                <div className="ex-stats-figure mg-1rem">Software Engineer</div>
                                 <div className="ex-stats-sub" >@ HCL Technologies</div>
 
                                 <div className="ex-description">Worked on high-stakes projects for industry giants PayPal and Western Union, crafting seamless digital solutions in the fintech space.</div>
                             </div>
-                            <div className="ex-container-sub">
-                                <div className="ex-box" id="ex-box-5">
-                                    <div className="ex-stats-figure">Frameworks in Action.</div>
-                                    <div className="ex-description">names in array to animate</div>
-                                </div>
-                                <div className="ex-box" id="ex-box-6">
-                                    <div className="ex-stats-figure">Clean, efficient Code</div>
-                                    {/* <img></img> */}
-                                </div>
+
+
+                        </div>
+                        <div className="ex-container-sub">
+                            <div className="ex-box flex-col" id="ex-box-7">
+                                <div className="ex-stats-figure"> Boosted engagement with responsive optimised UI/UX design.</div>
+                                {/* <img src="" alt="" /> */}
                             </div>
-                            <div className="ex-container-sub">
-                                <div className="ex-box" id="ex-box-7">
-                                    <div className="ex-stats-figure"> Boosted engagement with responsive optimised UI/UX design.</div>
-                                    {/* <img src="" alt="" /> */}
-                                </div>
-                                <div className="ex-box" id="ex-box-8">
+                            <div className="ex-box flex-col" id="ex-box-8">
                                 <div className="ex-stats-figure"> Blazing-fast performance with optimized efficiency.</div>
                                 {/* <img src="" alt="" /> */}
-                               </div>
+                            </div>
+                        </div>
+                        <div className="ex-container-sub">
+                            <div className="ex-box flex-col" id="ex-box-5">
+                                <div className="ex-stats-figure">Frameworks in Action.</div>
+                                <div className="ex-description">names in array to animate</div>
+                            </div>
+                            <div className="ex-box flex-col" id="ex-box-6">
+                                <div className="ex-stats-figure">Clean, efficient Code</div>
+                                {/* <img></img> */}
                             </div>
                         </div>
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,24 @@ import '../styles/Header.css';
 function Header() {
     return (
         <header className="intro-wrapper">
-            <div className="container flex-row">
-                <div className="intro-text-wrapper flex-col">
+            <div className="container flex-col">
+                <div className="intro-head-wrapper">
                     <h1>
-                        Hi ,<br /> I'm Shalini Kushwaha.
+                        welcome to
+                        <b> Shalini's </b>digital canvas  
                     </h1>
-                    <h3>A Full Stack Developer</h3>
-                    <h3>
-                        I design and build beautiful websites for businesses around the
-                        globe.
-                    </h3>
+                    <h2>
+                        - bringing creativity and functionality together..
+                    </h2>
                 </div>
-                <div className="intro-img-wrapper"></div>
+                <div className="intro-foot-wrapper flex-row ">
+                    <p>Hello! I'm passionate Full Stack Developer dedicated to building dynamic, user-centric web applications
+                        . Explore my portfolio to see how I blend innovation with technical skill. Let's create something amazing together! </p>
+                </div>
+
+                {/* <div className="intro-img-wrapper"></div> */}
             </div>
-        </header>
+        </header >
 
     );
 }
